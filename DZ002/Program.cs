bbf69c10d22c88f2fe1b8c25ba7c7315325db17b
Inna Osipova <rutheva_inna@mail.ru>
2022-07-07 18:23:14 +0300

    Сумма всех числе

---
+++ DZ002/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumNambers (int num)
+{
+    if (num == 0) return 0;
+    return  (num % 10) + SumNambers(num/10);
+    
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма всех цифр числа : "+SumNambers(number));
